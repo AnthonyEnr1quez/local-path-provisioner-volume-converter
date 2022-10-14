ec5531f70eb387115d4e8ff669889d843df20a02
AnthonyEnr1quez <32233059+AnthonyEnr1quez@users.noreply.github.com>
2022-10-14 14:57:01 -0500

    go fmt

--- internal/prompt/prompt.go
+++ internal/prompt/prompt.go
@@ -6,8 +6,8 @@ import (
 
 func AskOne(msg string, options []string, description func(value string, index int) string) (answer string) {
 	prompt := &survey.Select{
-		Message: msg,
-		Options: options,
+		Message:     msg,
+		Options:     options,
 		Description: description,
 	}
 	survey.AskOne(prompt, &answer)
