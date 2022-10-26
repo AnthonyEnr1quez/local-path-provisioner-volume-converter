264831e8768f05fb6b38d006782e08abe2324d16
AnthonyEnr1quez <32233059+AnthonyEnr1quez@users.noreply.github.com>
2022-10-26 12:22:16 -0500

    add go integration test

--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.19
 require (
 	github.com/AlecAivazis/survey/v2 v2.3.6
 	github.com/samber/lo v1.32.0
+	github.com/stretchr/testify v1.8.0
+	github.com/testcontainers/testcontainers-go v0.15.0
 	gopkg.in/yaml.v3 v3.0.1
 	k8s.io/api v0.25.2
 	k8s.io/apimachinery v0.25.2
@@ -12,38 +14,64 @@ require (
 )
 
 require (
+	github.com/Azure/go-ansiterm v0.0.0-20210617225240-d185dfc1b5a1 // indirect
+	github.com/Microsoft/go-winio v0.5.2 // indirect
+	github.com/Microsoft/hcsshim v0.9.4 // indirect
 	github.com/PuerkitoBio/purell v1.1.1 // indirect
 	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
+	github.com/cenkalti/backoff/v4 v4.1.3 // indirect
+	github.com/containerd/cgroups v1.0.4 // indirect
+	github.com/containerd/containerd v1.6.8 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/docker/distribution v2.8.1+incompatible // indirect
+	github.com/docker/docker v20.10.17+incompatible // indirect
+	github.com/docker/go-connections v0.4.0 // indirect
+	github.com/docker/go-units v0.5.0 // indirect
 	github.com/emicklei/go-restful/v3 v3.8.0 // indirect
 	github.com/go-logr/logr v1.2.3 // indirect
 	github.com/go-openapi/jsonpointer v0.19.5 // indirect
 	github.com/go-openapi/jsonreference v0.19.5 // indirect
 	github.com/go-openapi/swag v0.19.14 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/gnostic v0.5.7-v3refs // indirect
-	github.com/google/gofuzz v1.1.0 // indirect
+	github.com/google/gofuzz v1.2.0 // indirect
+	github.com/google/uuid v1.3.0 // indirect
 	github.com/imdario/mergo v0.3.12 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51 // indirect
+	github.com/magiconair/properties v1.8.6 // indirect
 	github.com/mailru/easyjson v0.7.6 // indirect
-	github.com/mattn/go-colorable v0.1.2 // indirect
-	github.com/mattn/go-isatty v0.0.8 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/mgutz/ansi v0.0.0-20170206155736-9520e82c474b // indirect
+	github.com/moby/sys/mount v0.3.3 // indirect
+	github.com/moby/sys/mountinfo v0.6.2 // indirect
+	github.com/moby/term v0.0.0-20210619224110-3f7ff695adc6 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/morikuni/aec v1.0.0 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.0.3-0.20211202183452-c5a74bcca799 // indirect
+	github.com/opencontainers/runc v1.1.3 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	go.opencensus.io v0.23.0 // indirect
 	golang.org/x/exp v0.0.0-20220303212507-bbda1eaf7a17 // indirect
 	golang.org/x/net v0.0.0-20220722155237-a158d28d115b // indirect
 	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8 // indirect
 	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
-	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
+	golang.org/x/term v0.0.0-20220526004731-065cf7ba2467 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	golang.org/x/time v0.0.0-20220609170525-579cf78fd858 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto v0.0.0-20220617124728-180714bec0ad // indirect
+	google.golang.org/grpc v1.47.0 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
---
+++ main_test.go
@@ -0,0 +1,229 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/AnthonyEnr1quez/local-path-provisioner-volume-converter/internal/kube"
+	"github.com/stretchr/testify/require"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/runtime/serializer/yaml"
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+func TestConversion(t *testing.T) {
+	ctx := context.Background()
+	requests := testcontainers.ParallelContainerRequest{
+		{
+			ContainerRequest: testcontainers.ContainerRequest{
+				Image:        "rancher/k3s:latest",
+				ExposedPorts: []string{"6443/tcp", "8443/tcp"},
+				Privileged:   true,
+				Cmd:          []string{"server"},
+				WaitingFor:   wait.ForLog("Node controller sync successful"),
+				Name:         "go-k3s",
+			},
+			Started: true,
+		},
+		{
+			ContainerRequest: testcontainers.ContainerRequest{
+				Image:      "fluxcd/flux-cli:v0.36.0",
+				Entrypoint: []string{"tail", "-f", "/dev/null"},
+				Name:       "go-flux",
+			},
+			Started: true,
+		},
+	}
+
+	res, err := testcontainers.ParallelContainers(ctx, requests, testcontainers.ParallelContainersOptions{WorkersCount: 2})
+	require.NoError(t, err)
+
+	for _, c := range res {
+		defer c.Terminate(ctx)
+	}
+
+	var k3sContainer, fluxContainer testcontainers.Container
+	for _, c := range res {
+		name, _ := c.Name(ctx)
+		if strings.Contains(name, "k3s") {
+			k3sContainer = c
+		} else if strings.Contains(name, "flux") {
+			fluxContainer = c
+		}
+	}
+
+	configBytes, err := getRawKubeConfig(ctx, k3sContainer)
+	require.NoError(t, err)
+
+	err = installFlux(ctx, configBytes, k3sContainer, fluxContainer)
+	require.NoError(t, err)
+
+	restConfig, err := getRestConfig(ctx, configBytes, k3sContainer)
+	require.NoError(t, err)
+
+	cs, err := kubernetes.NewForConfig(restConfig)
+	dc, err := dynamic.NewForConfig(restConfig)
+
+	err = updateProvisionerImage(cs)
+
+	// charts repo
+	_, err = createResourceFromFile(dc, "helmrepositories", "yaml/flux/charts.yaml")
+	// test ns
+	cs.CoreV1().Namespaces().Create(ctx, &corev1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: "test"}}, metav1.CreateOptions{})
+
+	cw := kube.TempCW(cs, dc)
+	// TODO
+	err = cw.CreateNamespace(kube.MigrationNamespace)
+	require.NoError(t, err)
+	defer cw.DeleteNamespace(kube.MigrationNamespace)
+	err = cw.CreateServiceAccount(kube.MigrationNamespace, kube.MigrationServiceAccount)
+	require.NoError(t, err)
+
+	tests := []struct {
+		resourceType      string
+		resourceLocation  string
+		pvcName           string
+		selectedNamespace string
+		selectedChartName string
+		volumeName        string
+		pvcNamespace      string
+	}{
+		{
+			resourceType:      "helmreleases",
+			resourceLocation:  "yaml/flux/helm-release.yaml",
+			pvcName:           "sonarr-config",
+			selectedNamespace: "test",
+			selectedChartName: "sonarr",
+			volumeName:        "config",
+			pvcNamespace:      "test",
+		},
+		{
+			resourceType:      "helmcharts",
+			resourceLocation:  "yaml/radarr.yaml",
+			pvcName:           "radarr-config",
+			selectedNamespace: "kube-system",
+			selectedChartName: "radarr",
+			volumeName:        "config",
+			pvcNamespace:      "test",
+		},
+	}
+	for _, test := range tests {
+		// test := test
+		t.Run(test.resourceType, func(t *testing.T) {
+			// t.Parallel()
+			selectedChart, err := createResourceFromFile(dc, test.resourceType, test.resourceLocation)
+			require.NoError(t, err)
+			exec(cw, selectedChart, test.pvcName, test.selectedNamespace, test.selectedChartName, test.volumeName, test.pvcNamespace)
+		})
+	}
+}
+
+func getRawKubeConfig(ctx context.Context, c testcontainers.Container) ([]byte, error) {
+	reader, err := c.CopyFileFromContainer(ctx, "/etc/rancher/k3s/k3s.yaml")
+	if err != nil {
+		return nil, err
+	}
+
+	return io.ReadAll(reader)
+}
+
+func getRestConfig(ctx context.Context, config []byte, k3sC testcontainers.Container) (*rest.Config, error) {
+	port, err := k3sC.MappedPort(ctx, "6443/tcp")
+	if err != nil {
+		return nil, err
+	}
+
+	host, err := k3sC.Host(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	restConfig, err := clientcmd.RESTConfigFromKubeConfig(config)
+	if err != nil {
+		return nil, err
+	}
+
+	restConfig.Host = fmt.Sprintf("https://%s:%s/", host, port.Port())
+
+	return restConfig, nil
+}
+
+func installFlux(ctx context.Context, config []byte, k3sC, fluxC testcontainers.Container) error {
+	k3sIP, err := k3sC.ContainerIP(ctx)
+	if err != nil {
+		return err
+	}
+
+	patchedConfig := bytes.ReplaceAll(config, []byte("127.0.0.1"), []byte(k3sIP))
+
+	err = fluxC.CopyToContainer(ctx, patchedConfig, "/kubeconfig", 700)
+	if err != nil {
+		return err
+	}
+
+	exitCode, output, err := fluxC.Exec(ctx, []string{"flux", "install", "--kubeconfig=kubeconfig"})
+	if err != nil {
+		return err
+	} else if exitCode != 0 {
+		out, err := io.ReadAll(output)
+		if err != nil {
+			return err
+		}
+		return errors.New(fmt.Sprintf("flux install failed: %s", out))
+	}
+	return nil
+}
+
+func updateProvisionerImage(cs kubernetes.Interface) (err error) {
+	deployments := cs.AppsV1().Deployments("kube-system")
+	lpp, err := deployments.Get(context.Background(), "local-path-provisioner", metav1.GetOptions{})
+	if err != nil {
+		return
+	}
+
+	lpp.Spec.Template.Spec.Containers[0].Image = "rancher/local-path-provisioner:v0.0.23"
+	_, err = deployments.Update(context.TODO(), lpp, metav1.UpdateOptions{})
+
+	return
+}
+
+func createResourceFromFile(dc dynamic.Interface, resourceType, fileName string) (unstructured.Unstructured, error) {
+	fileBytes, err := os.ReadFile(fileName)
+	if err != nil {
+		return unstructured.Unstructured{}, err
+	}
+
+	obj := &unstructured.Unstructured{}
+	dec := yaml.NewDecodingSerializer(unstructured.UnstructuredJSONScheme)
+	_, gvk, err := dec.Decode(fileBytes, nil, obj)
+	if err != nil {
+		return unstructured.Unstructured{}, err
+	}
+
+	namespace, found, err := unstructured.NestedString(obj.Object, "metadata", "namespace")
+	if err != nil {
+		return unstructured.Unstructured{}, err
+	}
+	if !found {
+		return unstructured.Unstructured{}, errors.New("namespace not found")
+	}
+
+	gvr := schema.GroupVersionResource{Group: gvk.Group, Version: gvk.Version, Resource: resourceType}
+	out, err := dc.Resource(gvr).Namespace(namespace).Create(context.TODO(), obj, metav1.CreateOptions{})
+
+	return *out, err
+}
