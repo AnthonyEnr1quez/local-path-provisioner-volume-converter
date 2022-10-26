f60a4a4a738787c59ab2fd98b0b69783d5bea8f1
AnthonyEnr1quez <32233059+AnthonyEnr1quez@users.noreply.github.com>
2022-10-26 13:33:44 -0500

    update flake

--- flake.nix
+++ flake.nix
@@ -1,6 +1,4 @@
 {
-  description = "Host to Local PVs";
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
@@ -9,14 +7,14 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = import nixpkgs { inherit system; };
-      in rec {
+      in rec
+      {
         packages = flake-utils.lib.flattenTree {
-          pv-to-local = let lib = pkgs.lib; in
-          pkgs.buildGoModule {
+          pv-to-local = pkgs.buildGoModule {
             pname = "pv-to-local";
             version = "0.0.1";
 
-            modSha256 = lib.fakeSha256;
+            modSha256 = pkgs.lib.fakeSha256;
             vendorSha256 = "iU0RRIL5Vkxvs9SqIxT3xhfiwBPIbhrjK1NuoqU/0wU=";
 
             src = ./.;
@@ -25,30 +23,26 @@
         };
 
         defaultPackage = packages.pv-to-local;
-        # defaultApp = packages.pv-to-local;
 
-        devShells = 
-        let pkgs = import nixpkgs { inherit system; };
-        in rec {
-          default = pkgs.mkShell {
-            buildInputs = with pkgs; [
-              go_1_19
-              gotools
-              gopls
-              go-outline
-              gocode
-              gopkgs
-              gocode-gomod
-              godef
-              golint
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            go_1_19
+            gotools
+            gopls
+            go-outline
+            gocode
+            gopkgs
+            gocode-gomod
+            godef
+            golint
 
-              delve
-              glibc
+            delve
+            glibc
 
-              kube3d
-              fluxcd
-            ];
-          };
+            kube3d
+            fluxcd
+            nixpkgs-fmt
+          ];
         };
       });
 }
